57dc273ccc221a5a65f7c481d7b4dad6d8eecedc
MagaBenG <ben--g2@hotmail.com>
2014-07-14 09:48:46 -0400

    Simian Main Push

---
+++ Simian/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Simian
+//
+//  Created by Benjamin DosSantos on 7/14/14.
+//  Copyright (c) 2014 BlackPenSoftware. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
